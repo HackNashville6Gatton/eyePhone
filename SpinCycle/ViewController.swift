5fe30893c0aecbb19ccee2312fa25d065d956a86
PeterKaminski09 <Peter.Kaminski09@gmail.com>
2014-11-08 09:48:21 -0600

    Initial Commit

---
+++ SpinCycle/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SpinCycle
+//
+//  Created by Peter Kaminski on 11/8/14.
+//  Copyright (c) 2014 WhiteSquirrelCoding. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
