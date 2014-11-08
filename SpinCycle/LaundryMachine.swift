0c692f9e65b58f988c31bc093b33020fb25df612
PeterKaminski09 <Peter.Kaminski09@gmail.com>
2014-11-08 10:59:01 -0600

    Laundry Class done. First version of storyboard.

---
+++ SpinCycle/LaundryMachine.swift
@@ -0,0 +1,48 @@
+//
+//  LaundryMachine.swift
+//  SpinCycle
+//
+//  Created by Peter Kaminski on 11/8/14.
+//  Copyright (c) 2014 WhiteSquirrelCoding. All rights reserved.
+//
+
+import Foundation
+
+class LaundryMachine: NSObject {
+    
+    //class variables
+    var number: Int //Number of the machine
+    var timeRemaining: Double //Time remaining for that particular machine's cycle
+    var watchCount: Int //How many people are currently attempting to use this machine
+    var typeMachine: Machine  //Is the machine a washer of a dryer? We will use 0 for washer, 1 for dryer
+    var inUse: Bool //Is it being used?
+    
+    //Constructor takes the number of the machine
+    init(number: Int, typeOfMachine: Machine){
+        
+        self.number = number
+        self.timeRemaining = 0
+        self.watchCount = 0
+        self.inUse = false
+        self.typeMachine = typeOfMachine
+        
+        
+    }
+    
+    //Starts the machine.
+    func start(cycleTime: Double){
+        self.timeRemaining = cycleTime
+        self.inUse = true
+        
+    }
+    
+    
+    //Enum to determine what type of machine we are using
+    enum Machine : Int{
+        case Washer = 0
+        case Dryer = 1
+    }
+    
+    
+    
+}
